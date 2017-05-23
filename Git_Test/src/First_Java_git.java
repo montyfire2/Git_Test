b1fe31f1ad1a33f17af79526256f48c5944ea7af
Blessan <Blessan@LAPTOP-H6J2RC0D>
2017-05-23 00:53:33 -0700

    Commit hello git and again write

---
+++ Git_Test/src/First_Java_git.java
@@ -0,0 +1,15 @@
+
+public class First_Java_git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello git");
+		
+		//second code
+		
+		System.out.println("againg write");
+
+	}
+
+}
