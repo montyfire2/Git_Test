021960445c1918880d350839483284d95748cb08
Gautam <Blessan@LAPTOP-H6J2RC0D>
2017-05-24 03:05:40 -0700

    Updated second.java

--- Git_Test/src/Second_java.java
+++ Git_Test/src/Second_java.java
@@ -4,7 +4,7 @@ public class Second_java {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Gautam");
+		System.out.println("montu");
 	}
 
 }
